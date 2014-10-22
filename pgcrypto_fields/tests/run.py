8051553aab8d68ac9d9dfd0eb2eb7e386a346d8f
Kevin Etienne <kevin@incuna.com>
2014-10-22 17:30:23 +0100

    Add test_requirements.txt

--- pgcrypto_fields/tests/run.py
+++ pgcrypto_fields/tests/run.py
@@ -2,9 +2,11 @@
 """From http://stackoverflow.com/a/12260597/400691."""
 import sys
 
-from colour_runner.django_runner import ColourRunnerMixin
 import dj_database_url
+import django
+from colour_runner.django_runner import ColourRunnerMixin
 from django.conf import settings
+from django.test.runner import DiscoverRunner
 
 
 settings.configure(
@@ -17,11 +19,9 @@ settings.configure(
     MIDDLEWARE_CLASSES = (),
 )
 
-import django
+
 if django.VERSION >= (1, 7):
     django.setup()
-
-from django.test.runner import DiscoverRunner
 
 
 class TestRunner(ColourRunnerMixin, DiscoverRunner):
