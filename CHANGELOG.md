07d45d8f5a3a3fa0b6e67e1ed6cc58cfb8d3caea
Kevin Etienne <kevin@incuna.com>
2015-01-13 15:22:59 +0000

    Bump to v0.6.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## Upcoming
+## v0.6.4
 
 * Remove `MaxLengthValidator` from email fields.
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 
-version = '0.6.3'
+version = '0.6.4'
 
 
 setup(
