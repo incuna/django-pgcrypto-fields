96924aea75dbbe82fec6c23df405a15e0bfeeac0
Kevin Etienne <kevin@incuna.com>
2016-10-07 15:52:18 +0100

    Exclude tests folder from dist

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0.0'
 
 setup(
     name='django-pgcrypto-fields',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests']),
     include_package_data=True,
     version=version,
     license='BSD',
