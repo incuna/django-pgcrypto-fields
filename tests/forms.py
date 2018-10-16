49bbe370c1ef0eb72503cb32ef6ddd6c4507ead5
peterfarrell <peter_farrell@saa.senate.gov>
2018-10-15 17:25:51 -0700

    cleanup

--- tests/forms.py
+++ tests/forms.py
@@ -14,6 +14,4 @@ class EncryptedForm(forms.ModelForm):
             'datetime_pgp_sym_field': forms.DateTimeInput(format='%m/%d/%Y %I:%M'),
             'date_pgp_pub_field': forms.DateInput(format='%m/%d/%Y'),
             'datetime_pgp_pub_field': forms.DateTimeInput(format='%m/%d/%Y %I:%M'),
-            'decimal_pgp_pub_field': forms.NumberInput(),
-            'decimal_pgp_sym_field': forms.NumberInput(),
         }
