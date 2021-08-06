30e76db917c966d774785065a542c643d6ab81ab
gongdao123 <gongdao123@gmail.com>
2021-08-06 17:23:12 +0800

    performance tune

--- pgcrypto/__init__.py
+++ pgcrypto/__init__.py
@@ -5,7 +5,7 @@ PGP_PUB_ENCRYPT_SQL_WITH_NULLIF = "pgp_pub_encrypt(nullif(%s, NULL)::text, dearm
 PGP_SYM_ENCRYPT_SQL_WITH_NULLIF = "pgp_sym_encrypt(nullif(%s, NULL)::text, '{}')"
 
 PGP_PUB_ENCRYPT_SQL = "pgp_pub_encrypt(%s, dearmor('{}'))"
-PGP_SYM_ENCRYPT_SQL = "pgp_sym_encrypt(%s, '{}')"
+PGP_SYM_ENCRYPT_SQL = "pgp_sym_encrypt(%s, '{}', 's2k-mode=1, s2k-digest-algo=md5')"
 
 PGP_PUB_DECRYPT_SQL = "pgp_pub_decrypt(%s, dearmor('{}'))::%s"
-PGP_SYM_DECRYPT_SQL = "pgp_sym_decrypt(%s, '{}')::%s"
+PGP_SYM_DECRYPT_SQL = "pgp_sym_decrypt(%s, '{}', 's2k-mode=1, s2k-digest-algo=md5')::%s"
