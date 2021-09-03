3b2e2ce60dce67971e63e4397a38f02e2355ec78
gongdao123 <gongdao123@gmail.com>
2021-09-03 23:15:37 +0800

    quick fix

--- pgcrypto/migrations/0002_add_function.py
+++ pgcrypto/migrations/0002_add_function.py
@@ -1,9 +1,8 @@
 from django.db import migrations
 
-FUNCTION_SQL = "
-CREATE OR REPLACE FUNCTION pgp_sym_decrypt_null_on_err(data bytea, psw text) RETURNS text AS $$
+FUNCTION_SQL = '''CREATE OR REPLACE FUNCTION pgp_sym_decrypt_null_on_err(data bytea, psw text, params text) RETURNS text AS $$
 BEGIN
-  RETURN pgp_sym_decrypt(data, psw);
+  RETURN pgp_sym_decrypt(data, psw, params);
 EXCEPTION
   WHEN external_routine_invocation_exception THEN
     RAISE DEBUG USING
@@ -13,7 +12,7 @@ EXCEPTION
        ERRCODE = 'external_routine_invocation_exception';
     RETURN NULL;
 END;
-$$ LANGUAGE plpgsql;"
+$$ LANGUAGE plpgsql;'''
 
 class Migration(migrations.Migration):
 
@@ -22,6 +21,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunSQL(FUNCTION_SQL)
+        migrations.RunSQL(sql=FUNCTION_SQL, reverse_sql=migrations.RunSQL.noop)
     ]
-
