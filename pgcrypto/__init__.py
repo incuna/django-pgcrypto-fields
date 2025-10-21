cbc97cc5e5c911f3ee6c6a7092577267e4fe409c
Andrea <andreabravetti@gmail.com>
2025-10-21 17:00:21 +0200

    Update __init__.py

--- pgcrypto/__init__.py
+++ pgcrypto/__init__.py
@@ -1,7 +1,7 @@
 DIGEST_SQL = "digest(%s, 'sha512')"
 HMAC_SQL = "hmac(%s, '{}', 'sha512')"
 
-PGP_PUB_ENCRYPT_SQL_WITH_NULLIF = "pgp_pub_encrypt(nullif(%s, NULL)::text, {}))"
+PGP_PUB_ENCRYPT_SQL_WITH_NULLIF = "pgp_pub_encrypt(nullif(%s, NULL)::text, {})"
 PGP_SYM_ENCRYPT_SQL_WITH_NULLIF = "pgp_sym_encrypt(nullif(%s, NULL)::text, '{}')"
 
 PGP_PUB_ENCRYPT_SQL = "pgp_pub_encrypt(%s, {})"
